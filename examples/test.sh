0a82c7a183a3a2fc6097582bd85f84eab9739f3a
Stefano <stefano@vargiuscuola.it>
2020-02-21 15:05:41 +0100

    2020-02-21 15:05:41] Add support to constants

---
+++ examples/test.sh
@@ -0,0 +1,27 @@
+# @file Title of file script
+# @brief Small description of the script.
+
+# @constant Color_Off Disable color
+# @constant Black Black color
+
+# @description Multiline description goes here and
+# there
+#
+# @example
+#   some:other:func a b c
+#   echo 123
+#
+# @arg $1 string Some arg.
+# @arg $@ any Rest of arguments.
+#
+# @noargs
+#
+# @exitcode 0  If successfull.
+# @exitcode >0 On failure
+# @exitcode 5  On some error.
+#
+# @stdout Path to something.
+#
+# @see some:other:func()
+some:first:func() {
+}
